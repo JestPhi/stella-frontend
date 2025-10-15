8a84c931efe073edeca1a0ba553ef76e3d9b4dd0
jestPhi <phi.le.fed@gmail.com>
2025-10-15 14:10:45 -0500

    clean up

--- app/config/firebase.ts
+++ app/config/firebase.ts
@@ -1,20 +1,14 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-// const firebaseConfig = {
-//   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-//   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-//   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-// };
-
 const firebaseConfig = {
-  apiKey: "AIzaSyDaa-a7VU1rnnv3lMHfZoKdVC4HdvMiA2I",
-  authDomain: "stella-ac7b9.firebaseapp.com",
-  projectId: "stella-ac7b9",
-  storageBucket: "stella-ac7b9.firebasestorage.app",
-  messagingSenderId: "1054908537886",
-  appId: "1:1054908537886:web:e7df8176786c29a520220f",
-  measurementId: "G-ZQHXBXP96J",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase app (singleton pattern)
