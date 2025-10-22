4245d35a3a56c85ae550bf829056a7cf93492f41
jestPhi <phi.le.fed@gmail.com>
2025-10-22 13:13:49 -0500

    remove unused code

--- app/components/Buttons/Home/index.tsx
+++ app/components/Buttons/Home/index.tsx
@@ -1,15 +1,12 @@
 "use client";
-import { useRouter } from "next/navigation";
+
 import Anchor from "../../Anchor";
 import Logo from "./logo";
 import styles from "./style.module.css";
 
 const Home = () => {
-  const router = useRouter();
   return (
-    <Anchor
-      href="/"
-    >
+    <Anchor href="/">
       <Logo className={styles.home} />
     </Anchor>
   );
